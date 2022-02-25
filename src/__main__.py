104b7eb59cddc779687af8e8e3a8cc6204516b30
Abelo98 <abelo08cruz@gmail.com>
2022-02-25 16:55:59 -0500

    replacing init methods testing1

--- src/__main__.py
+++ src/__main__.py
@@ -11,20 +11,20 @@ from pathlib import Path
 
 
 if __name__ == '__main__':
-    # if len(sys.argv) > 1:
-    #     input_file = sys.argv[1]
-    # with open(input_file, encoding="utf-8") as file:
-    #     text = file.read()
-
-    add = "semantic/arithmetic1.cl"
-
-    path: str = f"{Path.cwd()}/tests/{add}" if os.path.exists(
-        f"{Path.cwd()}/tests/{add}") else f"{Path.cwd()}/../tests/{add}"
-
-    _in = sys.argv[1] if len(sys.argv) > 1 else path
-    
-    with open(_in) as file:
+    if len(sys.argv) > 1:
+        input_file = sys.argv[1]
+    with open(input_file, encoding="utf-8") as file:
         text = file.read()
+
+    # add = "semantic/arithmetic1.cl"
+
+    # path: str = f"{Path.cwd()}/tests/{add}" if os.path.exists(
+    #     f"{Path.cwd()}/tests/{add}") else f"{Path.cwd()}/../tests/{add}"
+
+    # _in = sys.argv[1] if len(sys.argv) > 1 else path
+    
+    # with open(_in) as file:
+    #     text = file.read()
  
     lexer = main(text)            
     tokens = lexer.tokenize()
@@ -45,10 +45,10 @@ if __name__ == '__main__':
     context,scope = run_semantic_pipeline(ast)
     mips_output = run_code_gen_pipeline(ast,context,scope)
 
-    # with open(sys.argv[2], 'w') as f:
-    #     f.write(f'{mips_output}')
+    with open(sys.argv[2], 'w') as f:
+        f.write(f'{mips_output}')
 
-    with open(f'salida.s', 'w') as f:
-        f.write(mips_output)
+    # with open(f'salida.s', 'w') as f:
+    #     f.write(mips_output)
 
 
